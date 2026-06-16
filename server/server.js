8da1f4f5491d97543da7bcbdd3a8d8316b3e42fd
team3hailong <tranhailong2407@gmail.com>
2026-06-16 21:07:33 +0700

    first commit

--- server/server.js
+++ server/server.js
@@ -14,7 +14,10 @@ const PORT = process.env.PORT || 5000;
 
 connectDB();
 
-app.use(cors({ origin: process.env.CLIENT_URL || "http://localhost:5173", credentials: true }));
+app.use(cors({
+  origin: ['http://localhost:5173', 'http://localhost:3000', process.env.CLIENT_URL].filter(Boolean),
+  credentials: true
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
